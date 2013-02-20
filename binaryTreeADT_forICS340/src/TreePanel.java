467122179725577e742454aac254d6d5b835d39c
romeo mai <mnsaigonese@gmail.com>
2013-02-20 14:01:57 -0600

    First commit(test commit); added for loop to copy binaryTree queue over to q1 queue in paintComponet()

--- binaryTreeADT_forICS340/src/TreePanel.java
+++ binaryTreeADT_forICS340/src/TreePanel.java
@@ -26,26 +26,61 @@ public class TreePanel extends JPanel{
 		return (int)Math.pow(2.0, level);
 	}
 	
-	public void paintComponent(Graphics g){//This version needs to be rewritten
+	public void paintComponent(Graphics g){
 		//pre-process: input all the nodes into a queue (outputQueue) for easy output
 		super.paintComponent(g);
-		int boundingBox = 24, e, x, y, px, py, xText, yText;
+		int e;
+		int i = 0;
+		String str = "";
 		BTNode n;
-		Queue outputQueue = new Queue();  //this is one of those variables that is 
-                                                                                             //useful when drawing the whole tree
-		if(!binaryTree.isEmpty()){
-			//draw root
-			n = binaryTree.root();
+		Queue outputQueue = new Queue();
+		Queue q1 = new Queue(); //used to achieve level by level access of the tree
+		Queue q2 = new Queue();
+		if(!binaryTree.queue.isEmpty()){
+			for(i=0; i<binaryTree.queue.numOfNodes; i++){
+				q1.enque(binaryTree.queue.data[i]);			//add tree to queue (rmai)
+			}//for
+			while(!q1.isEmpty()||!q2.isEmpty()){
+				if(!q1.isEmpty()){
+					while(!q1.isEmpty()){
+						n = (BTNode)q1.deque();
+						if(n.getLeft()!=null){				//Fail at this line. null-exception
+							q2.enque(n.getLeft());
+						}
+						if(n.getRight()!=null){
+							q2.enque(n.getRight());
+						}
+						outputQueue.enque(n);	
+					}
+				}
+				if(!q2.isEmpty()){
+					while(!q2.isEmpty()){
+						n = (BTNode)q2.deque();
+						if(n.getLeft()!=null){
+							q1.enque(n.getLeft());
+						}
+						if(n.getRight()!=null){
+							q1.enque(n.getRight());
+						}
+						outputQueue.enque(n);
+					}
+				}
+				
+				
+			}//while
+			
+
+		}
+		
+		
+		
+		//output the tree in breadth first using one pass, taking all the nodes from the queue
+		while(!outputQueue.isEmpty()){
+			n = (BTNode)outputQueue.deque();
 			e = n.element();
-			x = n.getX();
-			x = x - boundingBox/2;  //do some minor adjusting
-			y = n.getY();
-			g.setColor(Color.black);
-			g.fillOval(x, y, boundingBox, boundingBox);
-			xText = x + boundingBox/3 - 5;
-			yText = y + boundingBox/2 + 5;
-			g.setColor(Color.white);
-			g.drawString(e+"", xText, yText);
+			str = str + e + "  ";
 		} 
+		g.drawString(str, 10, 100);
 	}
+
 }

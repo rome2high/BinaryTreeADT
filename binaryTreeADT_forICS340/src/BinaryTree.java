1aad338bea74713b3f753cf1dfcd2a71121a3a8f
romeo mai <mnsaigonese@gmail.com>
2013-02-21 18:21:34 -0600

    addNode continued; all child register root node as their parent.

--- binaryTreeADT_forICS340/src/BinaryTree.java
+++ binaryTreeADT_forICS340/src/BinaryTree.java
@@ -4,25 +4,25 @@ public class BinaryTree implements BinaryTreeADT {
 	protected  BTNode  root;
 	protected  int  size;
 	protected Queue queue; //used to keep track of last node.
-			    
+
 	public BinaryTree(){
 		root = null;
 		size = 0;
 		queue = new Queue();
 	}
-	
+
 	public BTNode root(){
 		if(!isEmpty())
 			return  root;
 		else return null;
 	}
-		
+
 	public boolean isEmpty() {
 		return (root == null);        
 	}
-			
+
 	public int size() {return  size;}
-		
+
 	public boolean isRoot(BTNode v) {
 		if(root == null)
 			return false;
@@ -35,7 +35,7 @@ public class BinaryTree implements BinaryTreeADT {
 		    return false;
 		else return true;
 	}
-			    
+
 	public BTNode parent(BTNode v){
 		if(hasParent(v))
 			return v.getParent();
@@ -45,11 +45,11 @@ public class BinaryTree implements BinaryTreeADT {
 	public boolean isLeaf(BTNode v) {
 		return (!hasLeft(v) && !hasRight(v));
 	}
-			
+
 	public boolean hasLeft(BTNode v) {
 		return  (v.getLeft() != null);
 	}
-		    
+
 	public BTNode left(BTNode v){
 		if(hasLeft(v))
 			return v.getLeft();
@@ -59,38 +59,39 @@ public class BinaryTree implements BinaryTreeADT {
 	public boolean hasRight(BTNode v) {
 		return  (v.getRight() != null);
 	}
-		    
+
 	public BTNode right(BTNode v){
 		if(hasRight(v))
 			return v.getRight();
 		else return null;
 	}
-		
+
 	public void addNode(int e) { 
 	    	BTNode n = new BTNode(e);
 	  	queue.enque(n); //insert n into queue; current node
 	    	//finish the rest
-	  		
-	  		BTNode frontNode = (BTNode) queue.peek();		//front node (first to go out)
-	  		
-	  		BTNode lastNode = (BTNode) queue.data[queue.rear - 2];
+  		BTNode frontNode = (BTNode) queue.peek();		//front node (first to go out)
 	  		
 	  	if(root == null){		//test if root exist
 	  		root = n;
-	  		n.level = 0;		//start off level\
+	  		n.x = 100;
+		  	n.y = 100;
+		  	n.px = 100;
+		  	n.py = 100;
+	  		n.level = 0;		//start off level
 	  	}
 	  	else
 	  	{
+	  		BTNode lastNode = (BTNode) queue.data[queue.rear - 2];
 		  	n.parent = root;
 		  	n.level = n.parent.level + 1;
+		  	n.x = n.parent.px - 30;
+		  	n.y = n.parent.py + 30;
 		  	System.out.println(n.getParent());
 	  	}
-	  	
+
 	  	//queue.deque();		//testing deque
 	  	
 	  	System.out.println(n.getParent());
-	  	
-
-
 	}         
 }
--- binaryTreeADT_forICS340/src/TreePanel.java
+++ binaryTreeADT_forICS340/src/TreePanel.java
@@ -26,61 +26,100 @@ public class TreePanel extends JPanel{
 		return (int)Math.pow(2.0, level);
 	}
 	
-	public void paintComponent(Graphics g){
+	public void paintComponent(Graphics g){//This version needs to be rewritten
 		//pre-process: input all the nodes into a queue (outputQueue) for easy output
 		super.paintComponent(g);
-		int e;
-		int i = 0;
-		String str = "";
+		int boundingBox = 24, e, x, y, px, py, xText, yText;
 		BTNode n;
-		Queue outputQueue = new Queue();
-		Queue q1 = new Queue(); //used to achieve level by level access of the tree
-		Queue q2 = new Queue();
-		if(!binaryTree.queue.isEmpty()){
-			for(i=0; i<binaryTree.queue.numOfNodes; i++){
-				q1.enque(binaryTree.queue.data[i]);			//add tree to queue (rmai)
-			}
-			while(!q1.isEmpty()||!q2.isEmpty()){
-				if(!q1.isEmpty()){
-					while(!q1.isEmpty()){
-						n = (BTNode)q1.deque();
-						if(n.getLeft()!=null){				//Fail at this line. null-exception
-							q2.enque(n.getLeft());
-						}
-						if(n.getRight()!=null){
-							q2.enque(n.getRight());
-						}
-						outputQueue.enque(n);	
-					}
-				}
-				if(!q2.isEmpty()){
-					while(!q2.isEmpty()){
-						n = (BTNode)q2.deque();
-						if(n.getLeft()!=null){
-							q1.enque(n.getLeft());
-						}
-						if(n.getRight()!=null){
-							q1.enque(n.getRight());
-						}
-						outputQueue.enque(n);
-					}
-				}
-				
-				
-			}//while
-			
+		Queue outputQueue = new Queue();  //this is one of those variables that is 
+                                          //useful when drawing the whole tree
+		
+		outputQueue = binaryTree.queue;
+		
+		if(!binaryTree.isEmpty()){
 
-		}
-		
-		
-		
-		//output the tree in breadth first using one pass, taking all the nodes from the queue
-		while(!outputQueue.isEmpty()){
-			n = (BTNode)outputQueue.deque();
+			//draw root
+			n = binaryTree.root();
 			e = n.element();
-			str = str + e + "  ";
+			x = n.getX();
+			x = x - boundingBox/2;  //do some minor adjusting
+			y = n.getY();
+			g.setColor(Color.black);
+			g.fillOval(x, y, boundingBox, boundingBox);
+			xText = x + boundingBox/3 - 5;
+			yText = y + boundingBox/2 + 5;
+			g.setColor(Color.white);
+			g.drawString(e+"", xText, yText);
+			
+			while(!outputQueue.isEmpty()){
+				n = (BTNode)outputQueue.deque();
+				e = n.element();
+				x = n.getX();
+				x = x - boundingBox/2;  //do some minor adjusting
+				y = n.getY();
+				g.setColor(Color.black);
+				g.fillOval(x, y, boundingBox, boundingBox);
+				xText = x + boundingBox/3 - 5;
+				yText = y + boundingBox/2 + 5;
+				g.setColor(Color.white);
+				g.drawString(e+"", xText, yText);
+			}
 		} 
-		g.drawString(str, 10, 100);
 	}
 
+
 }
+
+
+/* Test addNode code */
+
+//public void paintComponent(Graphics g){
+////pre-process: input all the nodes into a queue (outputQueue) for easy output
+//super.paintComponent(g);
+//int e;
+//int i = 0;
+//String str = "";
+//BTNode n;
+//Queue outputQueue = new Queue();
+//Queue q1 = new Queue(); //used to achieve level by level access of the tree
+//Queue q2 = new Queue();
+//
+//if(!binaryTree.queue.isEmpty()){
+//	for(i=0; i<binaryTree.queue.numOfNodes; i++){
+//		q1.enque(binaryTree.queue.data[i]);			//add tree to queue (rmai)
+//	}
+//	while(!q1.isEmpty()||!q2.isEmpty()){
+//		if(!q1.isEmpty()){
+//			while(!q1.isEmpty()){
+//				n = (BTNode)q1.deque();
+//				if(n.getLeft()!=null){				//Fail at this line. null-exception
+//					q2.enque(n.getLeft());
+//				}
+//				if(n.getRight()!=null){
+//					q2.enque(n.getRight());
+//				}
+//				outputQueue.enque(n);	
+//			}
+//		}
+//		if(!q2.isEmpty()){
+//			while(!q2.isEmpty()){
+//				n = (BTNode)q2.deque();
+//				if(n.getLeft()!=null){
+//					q1.enque(n.getLeft());
+//				}
+//				if(n.getRight()!=null){
+//					q1.enque(n.getRight());
+//				}
+//				outputQueue.enque(n);
+//			}
+//		}
+//	}//while
+//}
+////output the tree in breadth first using one pass, taking all the nodes from the queue
+//while(!outputQueue.isEmpty()){
+//	n = (BTNode)outputQueue.deque();
+//	e = n.element();
+//	str = str + e + "  ";
+//} 
+//g.drawString(str, 10, 100);
+//}
